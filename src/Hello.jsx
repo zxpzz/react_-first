8d609178aa8a9f36f14151ba6a87b1cb1e41450c
zxp <2997974917@qq.com>
2023-03-02 20:57:45 +0800

    first commit

---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+
+function Hello(test){
+    return(
+        <div>
+            <div>hello world</div>
+            <button onClick={()=>{alert(test.name)}}>点击</button>
+        </div>
+        
+    )
+}
+export default Hello
