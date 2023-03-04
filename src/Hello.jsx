395c82a7b06a0449d7d1c3c621addc5599c043a3
zxp <2997974917@qq.com>
2023-03-04 10:19:03 +0800

    commit

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,92 @@
-
-function Hello(test){
-    return(
-        <div>
-            <div>hello world</div>
-            <button onClick={()=>{alert(test.name)}}>点击</button>
+import React from "react"
+// function Hello(test){
+//     return(
+//         <div>
+//             <div>hello world</div>
+                // <h2>{this.name.toLocaleTimeString()}</h2>
+//             <button onClick={()=>{alert(test.name.toLocaleTimeString())}}>点击</button>
             
-        </div>
+//         </div>
         
-    )
+//     )
+// }
+
+// 将函数组件转换成 class 组件
+class Hello extends React.Component{
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       items: ['item 1', 'item 2', 'item 3']
+    //     };
+    //   }
+
+    //   render() {
+    //     const items = this.state.items.map((item, index) => (
+    //       <li key={index}>{item}</li>
+    //     ));
+      
+    //     return (
+    //       <div>
+    //         <ul>{items}</ul>
+    //       </div>
+    //     );
+    //   }
+
+    constructor(props){
+        super(props)
+        this.state={
+            data:new Date(),
+            users:[]
+        }
+    }
+
+    componentDidMount(){
+        this.time=setInterval(
+            ()=>this.tick(),1000
+        ),
+        this.fet()
+    }
+
+    fet(){
+        fetch("http://www.zxpzz.top:8081/users/")
+        .then(u=>u.json())
+        .then(u=>{
+            console.log(u)
+            // this.state.users=u
+            this.setState({
+               users:u
+            },()=>{
+                // 调用 setState 时，可能会出现异步更新的情况，
+                // 因此不能在调用 setState 后立即读取新的状态值。
+                // 如果需要读取新的状态值，可以使用 setState 的回调函数
+                console.log(this.state.users);
+            })
+        })
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.time)
+    }
+
+    tick(){
+        this.setState({
+            data:new Date()
+        })
+    }
+
+
+    render(){
+     
+        return(
+            <div>
+            <div>hello world</div>
+            <h2>{this.state.data.toLocaleTimeString()}</h2>
+            <button onClick={()=>{alert(this.state.data.toLocaleTimeString())}}>点击</button>
+            <ul>{this.state.users.map((item,index)=>(<li key={index}>{item.uname}</li>))}</ul>
+        </div>
+        )
+    }
+
 }
 export default Hello
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import './index.css'
 import Hello from './Hello'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Hello name="弹窗"/>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  //   {/* <Hello name={new Date()}/> */}
+  //   <Hello/>
+
+  // </React.StrictMode>,
+  <Hello />
 )
