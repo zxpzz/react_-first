99e04cb37e358e0f5baff59baaefd4b91ed0c06f
zxp <2997974917@qq.com>
2023-03-07 20:51:27 +0800

    first commit

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -16,10 +16,33 @@ import React from "react"
 
 function Test(username){
     return(
-        <div>hello test {username.name.map((users,index)=>(<li key={index}>{users.id}</li>))}</div>
+        <div>hello test {username.name.map((users)=>(<li key={users.id}>{users.age}</li>))}</div>
     )
 }
 
+function Warn(){
+    if(!status){
+        return null
+    }else{
+        return <h2 style={sty}>Warn</h2>
+    }
+}
+
+const sty={
+    color:'red'
+}
+
+function Status1(){
+    
+    if(status){
+        return <h2>true</h2>
+    }else{
+        return <h2>false</h2>
+    }
+}
+
+
+let status=false
 
 class Hello extends React.Component{
 
@@ -66,16 +89,56 @@ class Hello extends React.Component{
         })
     }
 
+    del(id,e){
+        alert(id)
+        alert(e)
+
+    }
+
+
+    del01(id){
+        alert(id)
+        
+    }
+
+    status1(){
+        status=!status
+    }
+
+
+
 
     render(){
-     
+     const test01=1
         return(
             <div>
+
+                
                 <div>hello world</div>
                 <h2>{this.state.data.toLocaleTimeString()}</h2>
                 <button onClick={()=>{alert(this.state.data.toLocaleTimeString())}}>点击</button>
-                <ul>{this.state.users.map((item,index)=>(<li key={index}>{item.uname}</li>))}</ul>
+                <ul>{this.state.users.map((item,index)=>(<li key={index}>
+                    {item.uname}
+                    {/* React 的事件对象 e 会被作为第二个参数传递。
+                    如果通过箭头函数的方式，事件对象必须显式的进行传递，
+                    而通过 bind 的方式，事件对象以及更多的参数将会被隐式的进行传递。 */}
+                    <button onClick={(e)=>{this.del(item.id,e)}}>test</button>
+                    <button onClick={this.del01.bind(this,item.id)}>test01</button>
+                </li>))}</ul>
 
+                {/* 条件渲染 */}
+                <button onClick={this.status1.bind(this)}>更改</button>
+                <Status1/>
+                <div>
+                    {
+                        test01>0&&<h2>test01 world</h2>
+                    } 
+                    {
+                        test01>0?'大于零':'小于零'
+                    }
+                </div>
+                <Warn />
+               
                 {/* 组件可以选择把它的 state 作为 props 向下传递到它的子组件中 */}
                 <Test name={this.state.users} />
             </div>
